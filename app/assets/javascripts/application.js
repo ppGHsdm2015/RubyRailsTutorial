ccdcd435a3577f2de876e2f856e08ee04dec7171
teamecho <premila1480@yahoo.co.in>
2015-10-21 06:17:21 +0000

    bug fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,53 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+function checkFullname(val) {
+    if (val == null || val == "") {
+        alert("Please fill in your full name.");
+        document.getElementById("txtname").focus();
+       return false;
+    }
+    //alert("The input value has changed. The new value is: ");
+}
+
+function checkEmail(val){
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    if (re.test(val)){
+        return true;
+    }
+    else{
+        alert("Please enter a valid email address.");
+        document.getElementById("txtemail").focus();
+        return false;
+    }
+    
+}
+
+ function checkPassword(val)
+  {
+    // at least one number, one lowercase and one uppercase letter
+    // at least six characters
+    var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}/;
+    if (re.test(val)){
+        return true;
+    }
+    else{ 
+        alert("Please enter a valid password with at least one number, one lowercase, one uppercase letter and between 6-15 characters");
+        document.getElementById("txtpwd").focus();
+        return false;
+    }
+  }
+  
+  function checkConPwd(val){
+      var pwd = document.getElementById("txtpwd").value;
+      alert(pwd);
+      if (pwd == val){
+          return true;
+      }
+      else{
+         alert("Password Confirmation does not match.");   
+          document.getElementById("txtemail").focus();
+      }
+  }
+
